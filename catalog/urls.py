ed391e49ea14788680c3f54eb78c5921b415544b
Olu Adesina <olu-adesina@hotmail.com>
2025-10-13 20:21:25 +0100

    feat(catalog/routes): add product list & detail routes with namespace

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+# catalog/urls.py
+from django.urls import path
+from . import views
+
+app_name = "catalog"
+
+urlpatterns = [
+    path("", views.product_list, name="product_list"),           # ← THIS is the list page
+    path("<slug:slug>/", views.product_detail, name="product_detail"),
+]
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
+# home/urls.py
 from django.urls import path
 from . import views
 
+app_name = "home"
+
 urlpatterns = [
-    path('', views.index, name='home'),
+    path("", views.index, name="home"),
 ]
